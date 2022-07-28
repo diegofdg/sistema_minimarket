b37d7ddc3f70f2d3397241db2b8f2b3e89655584
diegofdg <diegofdg@gmail.com>
2022-07-28 16:13:22 -0300

    creando las capas de categorías

---
+++ Sol_Minimarket/Sol_Minimarket.Datos/D_Categorias.cs
@@ -0,0 +1,41 @@
+﻿using Sol_Minimarket.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sol_Minimarket.Datos
+{
+    public class D_Categorias
+    {
+        public DataTable Listado_ca(string cTexto)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SQLCon = new SqlConnection();
+
+            try
+            {
+                SQLCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_Listado_ca", SQLCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@cTexto", SqlDbType.VarChar).Value = cTexto;
+                SQLCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (SQLCon.State == ConnectionState.Open) SQLCon.Close();
+            }
+        }        
+    }
+}
---
+++ Sol_Minimarket/Sol_Minimarket.Negocio/N_Categorias.cs
@@ -0,0 +1,21 @@
+﻿using Sol_Minimarket.Datos;
+using Sol_Minimarket.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sol_Minimarket.Negocio
+{
+    public class N_Categorias
+    {
+        public static DataTable Listado_ca(string cTexto)
+        {
+            D_Categorias Datos = new D_Categorias();
+            return Datos.Listado_ca(cTexto);
+        }
+    }
+        
+}
