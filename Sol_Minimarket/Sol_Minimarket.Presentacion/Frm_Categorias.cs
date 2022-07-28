27bdf1fb7e159d6129fa06195b7aae97b50670c2
diegofdg <diegofdg@gmail.com>
2022-07-28 15:57:07 -0300

    diseñando el formulario categorías

---
+++ Sol_Minimarket/Sol_Minimarket.Presentacion/Frm_Categorias.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sol_Minimarket.Presentacion
+{
+    public partial class Frm_Categorias : Form
+    {
+        public Frm_Categorias()
+        {
+            InitializeComponent();
+        }
+
+        #region "Mis Variables"
+        int Codigo_ca = 0;
+        int Estadoguarda = 0; //Sin ninguna acción
+        #endregion
+
+        #region "Mis Métodos"
+        private void Formato_ca()
+        {
+            
+        }
+
+        private void Listado_ca(string cTexto)
+        {
+            
+        }
+
+        private void Estado_Botonesprincipales(bool lEstado)
+        {
+            
+        }
+
+        private void Estado_Botonesprocesos(bool lEstado)
+        {
+            
+        }
+
+        private void Selecciona_item()
+        {
+            
+
+        }
+        #endregion
+
+        private void Frm_Categorias_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Btn_guardar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Btn_nuevo_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Btn_actualizar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Btn_cancelar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Dgv_principal_DoubleClick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Btn_retornar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Btn_eliminar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Btn_buscar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Btn_reporte_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Btn_salir_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
