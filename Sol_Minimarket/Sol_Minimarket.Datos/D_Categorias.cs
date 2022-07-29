264091ed6c92dadf9edefb40d300fc640301f0f0
diegofdg <diegofdg@gmail.com>
2022-07-28 21:31:29 -0300

    guardar categoría - capa de datos

--- Sol_Minimarket/Sol_Minimarket.Datos/D_Categorias.cs
+++ Sol_Minimarket/Sol_Minimarket.Datos/D_Categorias.cs
@@ -36,6 +36,33 @@ namespace Sol_Minimarket.Datos
             {
                 if (SQLCon.State == ConnectionState.Open) SQLCon.Close();
             }
-        }        
+        }
+
+        public string Guardar_ca(int nOpcion, E_Categorias oCa)
+        {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_Guardar_ca", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@nOpcion", SqlDbType.Int).Value = nOpcion;
+                Comando.Parameters.Add("@nCodigo_ca", SqlDbType.Int).Value = oCa.Codigo_ca;
+                Comando.Parameters.Add("@cDescripcion_ca", SqlDbType.VarChar).Value = oCa.Descripcion_ca;
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo registrar los datos";
+            }
+            catch (Exception ex)
+            {
+
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
+        }
     }
 }
