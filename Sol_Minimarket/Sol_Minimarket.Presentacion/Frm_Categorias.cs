f0c7dcda2773b1343c2bd692126fd8d9e7192b98
diegofdg <diegofdg@gmail.com>
2022-07-29 15:36:57 -0300

    diseño de reporte categorías

--- Sol_Minimarket/Sol_Minimarket.Presentacion/Frm_Categorias.cs
+++ Sol_Minimarket/Sol_Minimarket.Presentacion/Frm_Categorias.cs
@@ -193,7 +193,9 @@ namespace Sol_Minimarket.Presentacion
 
         private void Btn_reporte_Click(object sender, EventArgs e)
         {
-            
+            Reportes.Frm_Rpt_Categorias oRpt1 = new Reportes.Frm_Rpt_Categorias();
+            oRpt1.txt_p1.Text = Txt_buscar.Text;
+            oRpt1.ShowDialog();
         }
 
         private void Btn_salir_Click(object sender, EventArgs e)
